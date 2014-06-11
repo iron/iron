b17a00b4d6c3faa3078933a339442bcb2f76ead0
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-11 11:12:08 -0700

    (feat) Added boilerplate attributes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
-#![crate_id = ""]
-#![deny(missing_doc)]
+#![crate_id = "iron"]
+#![comment = "Rapid Web Development in Rust"]
+#![license = "MIT"]
 
-//! Documentation goes here.
+#![deny(missing_doc)]
+#![deny(unused_result)]
+#![deny(unnecessary_qualification)]
+#![deny(non_camel_case_types)]
+#![deny(unused_result)]
+#![deny(deprecated_owned_vector)]
+#![deny(unnecessary_typecast)]
+
+#![feature(macro_rules, phase)]
+
+//! The main crate for the Iron library.
 
