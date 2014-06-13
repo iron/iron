4edd9c64eaf00195617335ff9cfbe0ef2225f466
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-13 15:43:55 -0700

    (feat) Added Request trait, limited by rust-http.

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,1 +1,20 @@
-pub trait Request: Clone + Send {}
+use http::headers::request::HeaderCollection;
+use http::server::request::RequestUri;
+use http::method::Method;
+
+pub trait Request: Clone + Send {
+    fn headers_mut<'a>(&'a self)          -> &'a mut HeaderCollection;
+    fn body_mut<'a>(&'a self)             -> &'a mut String;
+    fn method_mut<'a>(&'a self)           -> &'a mut Method;
+    fn uri_mut<'a>(&'a self)              -> &'a mut RequestUri;
+    fn close_connection_mut<'a>(&'a self) -> &'a mut bool;
+
+    fn headers<'a>(&'a self)          -> &'a HeaderCollection;
+    fn body<'a>(&'a self)             -> String;
+    fn method<'a>(&'a self)           -> Method;
+    fn uri<'a>(&'a self)              -> RequestUri;
+    fn close_connection<'a>(&'a self) -> bool;
+
+    #[inline]
+    fn version(&self) -> (uint, uint) { (1, 1) }
+}
