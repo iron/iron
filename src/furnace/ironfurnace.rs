56b336807249b59e5afd7122813e277939e2be5b
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-13 10:48:45 -0700

    (feat) Added skeleton of forge for IronFurnace

--- src/furnace/ironfurnace.rs
+++ src/furnace/ironfurnace.rs
@@ -18,3 +18,24 @@ impl<Rq: Request, Rs: Response> Clone for Box<Ingot<Rq, Rs>> {
     fn clone(&self) -> Box<Ingot<Rq, Rs>> { self.clone_box() }
 }
 
+impl<Rq: Request, Rs: Response> Furnace<Rq, Rs> for IronFurnace<Rq, Rs> {
+    fn forge(&mut self, request: &mut Rq, response: &mut Rs, malloy: Option<&mut Alloy>) {
+        let mut alloy = &mut AnyMap::new();
+
+        match malloy {
+            Some(a) => alloy = a,
+            None => ()
+        };
+
+        'enter: for ingot in self.stack.mut_iter() {
+            match ingot.enter(request, response, alloy) {
+                Unwind   => break 'enter,
+                Continue => ()
+            }
+        }
+    }
+    fn smelt<I: Ingot<Rq, Rs>>(&mut self, ingot: I) {
+        self.stack.push(box ingot);
+    }
+}
+
