6c1be9718bd3369625771f07ab40a56f33df718e
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-13 10:47:03 -0700

    (feat) Updated the Furnace Trait.

--- src/furnace/mod.rs
+++ src/furnace/mod.rs
@@ -1,8 +1,12 @@
 use super::response::Response;
 use super::request::Request;
 use super::alloy::Alloy;
+use super::ingot::Ingot;
+
+pub mod ironfurnace;
 
 pub trait Furnace<Rq: Request, Rs: Response>: Send + Clone {
-    fn forge(&self, _request: &mut Rq, _response: &mut Rs, Option<&mut Alloy>);
+    fn forge(&mut self, _request: &mut Rq, _response: &mut Rs, Option<&mut Alloy>);
+    fn smelt<I: Ingot<Rq, Rs>>(&mut self, _ingot: I);
 }
 
