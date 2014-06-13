9cf4cb8a7f553f606d384f63bb04df4804fe90ed
zzmp <zmp@umich.edu>
2014-06-12 19:14:08 -0700

    (feat) Added alloy to ingot signatures.

--- src/ingot/mod.rs
+++ src/ingot/mod.rs
@@ -1,15 +1,16 @@
 use super::response::Response;
 use super::request::Request;
+use super::alloy::Alloy;
 
 pub enum Status {
     Continue, Unwind
 }
 
 pub trait Ingot<Rq: Request, Rs: Response>: Send + Clone {
-    fn enter(&mut self, _request: &mut Rq, _response: &mut Rs) -> Status {
+    fn enter(&mut self, _request: &mut Rq, _response: &mut Rs, _alloy: &mut Alloy) -> Status {
         Continue
     }
-    fn exit(&mut self, _request: &mut Rq, _response: &mut Rs) -> Status {
+    fn exit(&mut self, _request: &mut Rq, _response: &mut Rs, _alloy: &mut Alloy) -> Status {
         Continue
     }
 }
