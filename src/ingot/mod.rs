d4cec762cb0078574f9d33df1b04f0a77c51d667
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-14 23:06:55 -0700

    (doc) Documented ingot::Status

--- src/ingot/mod.rs
+++ src/ingot/mod.rs
@@ -2,8 +2,27 @@ use super::response::Response;
 use super::request::Request;
 use super::alloy::Alloy;
 
+/// The Status returned by `Ingot's` `enter` and `exit` methods. This indicates
+/// to the `Furnace` whether this is a terminal `Ingot` or whether to continue
+/// passing the `Request` and `Response` down the `Furnace's` stack.
+///
+/// Most `Furnaces` will ignore the returned `Status` from the `exit` method of
+/// `Ingots`.
 pub enum Status {
-    Continue, Unwind
+    /// `Continue` indicates that this is an intermediate `Ingot` in the stack
+    /// and the `Furnace` should continue passing requests down the `Furnace's`
+    /// stack.
+    ///
+    /// Most `Ingots` will return `Continue` from both `enter` and `exit`.
+    Continue,
+
+    /// `Unwind` indicates that this is a terminal `Ingot` or something went
+    /// wrong. It can be used to immediately stop passing requests down the
+    /// `Furnace's` stack and start calling `exit` of all previous `Ingots`.
+    ///
+    /// For instance, an authorization `Ingot` might return `Unwind` if the
+    /// request fails an authentication check, and `Continue` otherwise.
+    Unwind
 }
 
 pub trait Ingot<Rq: Request, Rs: Response>: Send + Clone {
