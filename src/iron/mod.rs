af99946604d2f8fc1aa76434e862e5072c07e163
zzmp <zmp@umich.edu>
2014-06-14 23:17:52 -0700

    (doc) Documented iron/mod.

--- src/iron/mod.rs
+++ src/iron/mod.rs
@@ -17,7 +17,24 @@ pub type ServerT =
     Iron<IronRequest, IronResponse<'static, 'static>,
          IronFurnace<IronRequest, IronResponse<'static, 'static>>>;
 
+/// The primary entrance point to `Iron`, a `struct` to instantiate a new server.
+///
+/// This is entrance point to `Iron`, from which you create a server. The server
+/// can be made with a specific `Furnace` (using `from_furnace`) or with a new on
+/// (`new`). `Iron` is used to manage the server processes:
+/// `Iron.smelt` is used to add new `Ingot`s, and
+/// `Iron.listen` is used to kick off a server process.
+///
+/// `Iron` contains the `Furnace` which holds the `Ingot`s necessary to run a server.
+/// `Iron` is the main interface to adding `Ingot`s, and has `Furnace as public for
+/// (for the sake of extensibility).
 pub struct Iron<Rq, Rs, F> {
+    /// Exposed internal storage of a `Furnace`.
+    ///
+    /// Set furnace to implement your server's middleware stack with custom behavior.
+    /// Most uses will not need to touch `furnace`. This should only be used if you
+    /// need custom handling of `Ingot`s. Normally, the default `IronFurnace` is
+    /// sufficient.
     pub furnace: F,
     ip: Option<IpAddr>,
     port: Option<u16>
@@ -35,16 +52,36 @@ impl<Rq, Rs, F: Clone> Clone for Iron<Rq, Rs, F> {
 
 impl<Rq: Request, Rs: Response, F: Furnace<Rq, Rs>>
         Iron<Rq, Rs, F> {
+    /// `smelt` a new `Ingot`.
+    ///
+    /// This adds an `Ingot` to the `Iron`'s `furnace`, so that any requests
+    /// are passed through those `Ingot`s.
+    ///
+    /// `Iron.smelt` delegates to `iron.furnace.smelt`, so that any `Ingot`
+    /// added is added to the `Iron` instance's `furnace`.
     pub fn smelt<I: Ingot<Rq, Rs>>(&mut self, ingot: I) {
         self.furnace.smelt(ingot);
     }
 
+    /// Kick off the server process.
+    ///
+    /// Call this once to begin listening for requests on the server.
+    /// This is a blocking operation, and is the final op that should be called
+    /// on the `Iron` instance. Once `listen` is called, requests will be
+    /// handled as defined through the `Iron`'s `furnace`'s `Ingot`s.
     pub fn listen(mut self, ip: IpAddr, port: u16) {
         self.ip = Some(ip);
         self.port = Some(port);
         self.serve_forever();
     }
 
+    /// Instantiate a new instance of `Iron`.
+    ///
+    /// This will create a new `Iron`, the base unit of the server.
+    /// This creates an `Iron` with a default `furnace`, the `IronFurnace`.
+    /// This `furnace` will serve requests until it hits an `Unwind`, and then
+    /// exit through the `Ingot`s it has gone through. Custom furnaces can be
+    /// used with `from_furnace`, instead of `new`.
     pub fn new<Rq, Rs>() -> Iron<Rq, Rs, F> {
         let furnace = Furnace::new();
         Iron {
@@ -54,6 +91,16 @@ impl<Rq: Request, Rs: Response, F: Furnace<Rq, Rs>>
         }
     }
 
+    /// Instantiate a new isntance of `Iron` from an existing `Furnace`.
+    ///
+    /// This will create a new `Iron` from a give `Furnace`.
+    /// The `Furnace` can be configured to handle `Ingot`s differently than
+    /// `IronFurnace`. For example, this can be used to implement a `Furnace`
+    /// that logs debug messages as it serves requests.
+    ///
+    /// Most uses will not need to touch `from_furnace`. This should only be used if you
+    /// need custom handling of `Ingot`s. Normally, the default `IronFurnace` is
+    /// sufficient.
     pub fn from_furnace<Rq, Rs, F>(furnace: F) -> Iron<Rq, Rs, F> {
         Iron {
             furnace: furnace,
@@ -63,6 +110,7 @@ impl<Rq: Request, Rs: Response, F: Furnace<Rq, Rs>>
     }
 }
 
+/// This `impl` allows `Iron` to be used as a `Server` by [rust-http]('https://github.com/chris-morgan/rust-http'). It is not used by a consumer.
 impl<Rq: Request,
      Rs: Response,
      F: Furnace<Rq, Rs>>
