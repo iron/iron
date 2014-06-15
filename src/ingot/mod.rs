78e71dc0c931c2567c9c3ac8625223bf36d903ee
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-14 23:20:27 -0700

    (doc) Documented Ingot trait.

--- src/ingot/mod.rs
+++ src/ingot/mod.rs
@@ -25,6 +25,27 @@ pub enum Status {
     Unwind
 }
 
+/// All middleware should implement `Ingot`, which allows it to be `smelted`
+/// to a `Furnace` so that it will be called for each incoming request.
+///
+/// There are two sorts of data associated with `Ingots`, data internal
+/// to the `Ingot` and APIs that the `Ingot` would like to expose to `Ingots`
+/// further down the stack or terminal controllers.
+///
+/// Internal data should be stored on the `struct` that implements `Ingot`
+/// itself. All `Ingots` are cloned for each client request, so the object
+/// initially smelted on to the `Iron` instance will be provided as `&self` to
+/// enter for every request. Data stored on `Ingot` instances does _not_ persist
+/// between requests and is _not_ shared between different, concurrent, requests.
+/// The same is true for data stored on `Alloys`.
+///
+/// External data should be stored in the `Alloy` passed to both `enter` and
+/// `exit`. `Alloy` is a thin wrapper around `AnyMap` and is effectively a
+/// a key value store from a type to an instance of that type. This means
+/// that each `Ingot` can have a unique type that it stores in the `Alloy`.
+/// This can either be an instance of that `Ingot` or some other type. Since
+/// the same `Alloy` is passed to all further `Ingots` in the `Furnace`, this
+/// scheme allows you to expose data or functionality to future `Ingots`.
 pub trait Ingot<Rq: Request, Rs: Response>: Send + Clone {
     fn enter(&mut self, _request: &mut Rq, _response: &mut Rs, _alloy: &mut Alloy) -> Status {
         Continue
