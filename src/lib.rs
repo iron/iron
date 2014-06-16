875f8d000a97da349f71c29335a12ff8c024b2d1
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 11:05:43 -0700

    (init)

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[crate_id = "iron-router"]
+#![license = "MIT"]
+//#![deny(missing_doc)]
+#![deny(unused_result, unused_result, unnecessary_qualification,
+        non_camel_case_types, unused_variable, unnecessary_typecast)]
+
