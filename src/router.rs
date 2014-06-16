39fe6f15b4a9ac9286e0a84513376ffdfa7309db
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 12:55:02 -0700

    (feat) Added routing methods.

--- src/router.rs
+++ src/router.rs
@@ -26,5 +26,38 @@ impl Router {
         }
         self.routes.push(route);
     }
+    method!(get, Get)
+    method!(post, Post)
+    method!(put, Put)
+    method!(patch, Patch)
+    method!(delete, Delete)
+}
+
+macro_rules! method {
+    ($name:ident, $method:ident) => {
+        fn $name(&mut self, matches: Regex, handler: Handler) {
+            self.addRoute(Route {
+                method: $method
+                matches: matches,
+                handler: handler
+            });
+        }
+    }
+}
+
+impl<Rq: Request, Rs: Response> Ingot<Rq, Rs> for Router {
+    fn enter(&mut self, req: &mut Rq, res: &mut Rs, alloy: &mut Alloy) -> Status {
+        if *req.method() == Options {
+            res.write(self.options.iter().map(|p| p.show()).collect().join(" "));
+            return Unwind;
+        }
+        for route in self.routes.iter() {
+            if route.matches.is_match(req.reques_uri()) {
+                route.handler(req, res, alloy);
+                return Unwind;
+            }
+        }
+        Continue
+    }
 }
 
