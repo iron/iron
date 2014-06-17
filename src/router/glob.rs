fbfb181ad1f8ac7a31fcf4b71488432a932fdecb
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 20:12:43 -0700

    (test) Added tests for de-globbing of routes.

--- src/router/glob.rs
+++ src/router/glob.rs
@@ -19,3 +19,50 @@ pub fn deglob(glob: String) -> Regex {
     Regex::new("^".to_string().append(debound.as_slice()).append("$").as_slice()).unwrap()
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_segment_match() {
+        // Do literal matches work?
+        let glob_regex = deglob("/users/nested/groups".to_string());
+        assert!(glob_regex.is_match("/users/nested/groups"));
+        assert!(!glob_regex.is_match("/notusers/hello"))
+    }
+
+    #[test]
+    fn test_wildcard_match() {
+        // Do wilcard matches work?
+        let glob_regex = deglob("/users/*/groups".to_string());
+        assert!(glob_regex.is_match("/users/nested/groups"));
+        assert!(glob_regex.is_match("/users//groups"));
+        assert!(!glob_regex.is_match("/users/deeply/nested/groups"));
+    }
+
+    #[test]
+    fn test_double_wildcard_match() {
+        // Do double wildcard matches work?
+        let glob_regex = deglob("/users/**/groups".to_string());
+        assert!(glob_regex.is_match("/users/deeply/nested/groups"));
+        assert!(glob_regex.is_match("/users//groups"));
+        assert!(!glob_regex.is_match("/notusers/groups/"))
+    }
+
+    #[test]
+    fn test_params_match() {
+        // Does param matching work?
+        let glob_regex = deglob("/users/:groupid/:userid".to_string());
+        assert!(glob_regex.is_match("/users/73564/87684"));
+        assert!(!glob_regex.is_match("/users/234/groups/2343"));
+    }
+
+    #[test]
+    fn test_params_value() {
+        // Do params have the correct value?
+        let glob_regex = deglob("/users/:groupid/:userid".to_string());
+        assert_eq!(glob_regex.captures("/users/7374/234").unwrap().name("userid"), "234");
+        assert_eq!(glob_regex.captures("/users/7374/234").unwrap().name("groupid"), "7374");
+    }
+}
+
