c757cfbe3d0c3805427d24266552c812dfd03fdd
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 18:08:42 -0700

    (fix) Updated broken attributes and dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
-#[crate_id = "iron-router"]
+#![crate_id = "iron-router"]
 #![license = "MIT"]
 //#![deny(missing_doc)]
 #![deny(unused_result, unused_result, unnecessary_qualification,
         non_camel_case_types, unused_variable, unnecessary_typecast)]
-#![feature(phase, macro_export)]
+#![feature(phase, globs, macro_rules)]
 
 extern crate http;
 extern crate iron;
 extern crate regex;
+#[phase(plugin, link)] extern crate log;
 #[phase(plugin)] extern crate regex_macros;
 
 pub use router::Router;
+pub use router::Handler;
 
 mod router;
+
