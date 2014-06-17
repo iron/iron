5362fbf5ff924f7c710660cba82573a01017d813
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-17 16:17:24 -0700

    (fix) Removed macro_rules feature flag.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //#![deny(missing_doc)]
 #![deny(unused_result, unused_result, unnecessary_qualification,
         non_camel_case_types, unused_variable, unnecessary_typecast)]
-#![feature(phase, globs, macro_rules)]
+#![feature(phase, globs)]
 
 extern crate http;
 extern crate iron;
