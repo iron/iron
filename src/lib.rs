7baea75615b40525db6a3074456e8c3265b5ffd1
Michael Reinhard <mcreinhard@gmail.com>
2014-06-16 18:11:14 -0700

    (feat) Added clean logging of non-header fields.

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,34 @@
 
 //! Request logging middleware for Iron
 
-extern crate debug;
 extern crate iron;
+extern crate time;
 
 use iron::{Ingot, Alloy, Request, Response};
 use iron::ingot::{Status, Continue};
 
+use time::precise_time_ns;
+
 #[deriving(Clone)]
-pub struct Logger;
+pub struct Logger {
+    entry_time: u64
+}
 
 impl Logger {
     pub fn new() -> Logger {
-        Logger
+        Logger { entry_time: 0u64 }
     }
 }
 
 impl<Rq: Request, Rs: Response> Ingot<Rq, Rs> for Logger {
     fn enter(&mut self, _req: &mut Rq, _res: &mut Rs, _alloy: &mut Alloy) -> Status {
+        self.entry_time = precise_time_ns();
         Continue
     }
-    fn exit(&mut self, req: &mut Rq, _res: &mut Rs, _al: &mut Alloy) -> Status {
-        println!("{:?}", req);
+    fn exit(&mut self, req: &mut Rq, res: &mut Rs, _al: &mut Alloy) -> Status {
+        let response_time_ms = (precise_time_ns() - self.entry_time) as f64 / 1000000.0;
+        println!("Request body: {}\nMethod: {}\nURI: {}\nStatus: {}\nResponse time: {} ms\n",
+                 req.body(), req.method(), req.uri(), res.status(), response_time_ms);
         Continue
     }
 }
