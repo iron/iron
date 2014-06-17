3bacb6bca95b499a937e92789b659095ac3fb5b5
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 18:22:54 -0700

    (feat) Added deglob function.

--- src/router.rs
+++ src/router.rs
@@ -69,3 +69,20 @@ impl<Rq: Request, Rs: Response> Ingot<Rq, Rs> for Router {
     }
 }
 
+static PARAMS: Regex = regex!(r":([a-zA-Z0-9_-]*)");
+
+fn deglob(glob: String) -> Regex {
+    // Replace glob patterns with corresponding regexs.
+    let deglobbed = glob
+        // Have to do this because the ** regex contains *
+        .replace("**", "___DOUBLE_WILDCARD___")
+        // Now only __DOUBLE_WILDCARD___ remains.
+        .replace("*", "[a-zA-Z0-9_-]*")
+        // Replace ** with its associated regex.
+        .replace("___DOUBLE_WILDCARD___", "[/a-zA-Z0-9_-]*");
+    // Replace :param patterns with corresponding regexs.
+    let debound = PARAMS
+        .replace_all(deglobbed.as_slice(), "([a-zA-Z0-9_-]*)");
+    Regex::new("^".to_string().append(debound.as_slice()).append("$").as_slice()).unwrap()
+}
+
