8d8117963c8c99d5271a53e7dcaa4360ce258d7a
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-16 20:55:34 -0700

    (test) Added tests and benching of Params.

--- src/router/params.rs
+++ src/router/params.rs
@@ -13,8 +13,29 @@ impl Params {
         }
     }
 
-    pub fn get<'a>(&'a self, param: String) -> Option<&'a String> {
-        self.captures.find(&param)
+    pub fn get<'a>(&'a self, param: &'static str) -> Option<String> {
+        self.captures.find(&param.to_string()).and_then(|p| Some(p.clone()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use test::{Bencher, black_box};
+    use super::*;
+    use super::super::glob::deglob;
+
+    #[test]
+    fn test_new() {
+        let params = Params::new("/users/7324", deglob("/users/:userid".to_string()), vec!["userid".to_string()].move_iter());
+        assert_eq!(params.get("userid").unwrap(), "7324".to_string());
+    }
+
+    #[bench]
+    fn bench_get(b: &mut Bencher) {
+        let params = Params::new("/users/7324", deglob("/users/:userid".to_string()), vec!["userid".to_string()].move_iter());
+        b.iter(|| {
+            black_box(params.get("userid").unwrap())
+        })
     }
 }
 
