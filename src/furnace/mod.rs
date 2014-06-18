3001b01ee9e16f488375f72c37886a9473ec7cbb
zzmp <zmp@umich.edu>
2014-06-18 00:12:55 -0700

    (doc) Updated Furnace for Middleware.

--- src/furnace/mod.rs
+++ src/furnace/mod.rs
@@ -1,4 +1,4 @@
-//! Exposes the `Furnace` trait and `IronFurnace` type.
+//! Exposes the `Furnace` trait and `StackFurnace` type.
 
 use super::response::Response;
 use super::request::Request;
@@ -7,32 +7,30 @@ use super::middleware::Middleware;
 
 pub mod stackfurnace;
 
-/// `Furnaces` are the backbone of `Iron`. They coordinate `Ingots`
-/// and ensure they are resolved and called in the right order,
+/// `Furnaces` are the backbone of `Iron`. They coordinate `Middleware`
+/// to ensure they are resolved and called in the right order,
 /// create and distribute `Alloys`, and handle incoming requests.
 ///
 /// `Furnaces` are internal tools. Unless you want additional
 /// or unusual behavior such as enhanced debug logging you
 /// probably don't need to mess with `Furnace` internals.
 ///
-/// That being said, custom `Furnaces` are extremely powerful as
-/// they allow you to completely control the resolution of `Ingots`.
-///
-/// The default `Furnace` used in `Iron` is the `IronFurnace`.
+/// That being said, custom `Furnaces` are extremely powerful.
+/// They allow you to completely control the resolution of `Middleware`.
 pub trait Furnace: Send + Clone {
-    /// A `Furnace's` forge method will get called once per Request, and may be
+    /// `forge` will be called once per `Request`, and may be
     /// called either with or without an existing `Alloy`. `Forge` is responsible
-    /// for delegating the request to the correct `Ingots` and in the correct
+    /// for delegating the request to the correct `Middleware` and in the correct
     /// order. Effectively, 99% of the work done by a `Furnace` is done here.
     fn forge(&mut self,
              _request: &mut Request,
              _response: &mut Response,
              Option<&mut Alloy>);
 
-    /// `Smelt` is responsible for adding an `Ingot` to the `Furnace's` internal
-    /// storage of `Ingots`. Different `Furnaces` may implement different behavior
-    /// for smelt, but ideally an `Ingot` added here will be delegated to during
-    /// Requests.
+    /// `smelt` is responsible for adding new `Middleware` to the `Furnace's` internal
+    /// storage of `Middleware`. Different `Furnaces` may implement different behavior
+    /// for `smelt`, but - ideally - `Middleware` added here will be delegated to during
+    /// `Requests`.
     fn smelt<I: Middleware>(&mut self, _middleware: I);
 
     /// Create a new instance of `Furnace`.
