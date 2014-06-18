a97ab09d7912c1df1403757e1ac68c20e1d1f619
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-17 23:06:00 -0700

    (refactor) Refactored Furnace with Request and Response types.

--- src/furnace/mod.rs
+++ src/furnace/mod.rs
@@ -5,7 +5,7 @@ use super::request::Request;
 use super::alloy::Alloy;
 use super::ingot::Ingot;
 
-pub mod ironfurnace;
+pub mod stackfurnace;
 
 /// `Furnaces` are the backbone of `Iron`. They coordinate `Ingots`
 /// and ensure they are resolved and called in the right order,
@@ -19,18 +19,21 @@ pub mod ironfurnace;
 /// they allow you to completely control the resolution of `Ingots`.
 ///
 /// The default `Furnace` used in `Iron` is the `IronFurnace`.
-pub trait Furnace<Rq: Request, Rs: Response>: Send + Clone {
+pub trait Furnace: Send + Clone {
     /// A `Furnace's` forge method will get called once per Request, and may be
     /// called either with or without an existing `Alloy`. `Forge` is responsible
     /// for delegating the request to the correct `Ingots` and in the correct
     /// order. Effectively, 99% of the work done by a `Furnace` is done here.
-    fn forge(&mut self, _request: &mut Rq, _response: &mut Rs, Option<&mut Alloy>);
+    fn forge(&mut self,
+             _request: &mut Request,
+             _response: &mut Response,
+             Option<&mut Alloy>);
 
     /// `Smelt` is responsible for adding an `Ingot` to the `Furnace's` internal
     /// storage of `Ingots`. Different `Furnaces` may implement different behavior
     /// for smelt, but ideally an `Ingot` added here will be delegated to during
     /// Requests.
-    fn smelt<I: Ingot<Rq, Rs>>(&mut self, _ingot: I);
+    fn smelt<I: Ingot>(&mut self, _ingot: I);
 
     /// Create a new instance of `Furnace`.
     /// If you are making your own furnace, you'll need to
