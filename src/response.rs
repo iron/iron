c7614513c83ce34655c707b01f2909632b45d351
zzmp <zmp@umich.edu>
2014-06-18 00:49:57 -0700

    (feat) Added ServeFile trait to Response.

--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,17 @@
+use std::io::{IoResult, File};
+use std::io::util::copy;
+
 //! An alias of the rust-http Response struct.
 
 pub use Response = http::server::response::ResponseWriter;
+
+pub trait ServeFile: Writer {
+    fn serve_file(&mut self, &Path) -> IoResult<()>;
+}
+
+impl<'a> ServeFile for Response<'a> {
+    fn serve_file(&mut self, path: &Path) -> IoResult<()> {
+        let mut file = try!(File::open(path));
+        copy(&mut file, self)
+    }
+}
