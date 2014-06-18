64301c1d114bca0f97616695dba50b75f8a46a77
zzmp <zmp@umich.edu>
2014-06-18 12:31:44 -0700

    (doc) Documented ServeFile trait.

--- src/response.rs
+++ src/response.rs
@@ -1,11 +1,22 @@
+//! An augmentation of the rust-http Response struct.
+
 use std::io::{IoResult, File};
 use std::io::util::copy;
 
-//! An alias of the rust-http Response struct.
-
 pub use Response = http::server::response::ResponseWriter;
 
+/// Allow file-serving
 pub trait ServeFile: Writer {
+    /// Serve the file located at `path`.
+    ///
+    /// This is usually a terminal process, and `Middleware` may want to
+    /// call `Unwind` after a file is served. If the status should be
+    /// anything other than `200`, the `Middleware` must set it, including in
+    /// the case of an `Err`.
+    ///
+    /// `serve_file` will err out if the file does not exist, the process
+    /// does not have correct permissions, or it has other issues in reading
+    /// from the file. Middleware should handle this gracefully.
     fn serve_file(&mut self, &Path) -> IoResult<()>;
 }
 
