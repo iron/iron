539b76b02b2d10b3faf4573c0c2a1ca760f8346f
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-19 13:52:59 -0700

    (feat) Added getter for the url for Request objects.

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,24 @@
 //! An alias of the rust-http Request struct.
 
+use http::server::request::{AbsolutePath};
 pub use Request = http::server::request::Request;
+
+/// Adds a url getter method.
+pub trait GetUrl {
+    /// A url getter method for requests or responses.
+    fn url<'a>(&'a self) -> Option<&'a String>;
+}
+
+impl GetUrl for Request {
+    /// Get the url from a Request
+    ///
+    /// Returns Some(&url) if this is an AbsolutePath
+    /// request, otherwise it returns None.
+    fn url<'a>(&'a self) -> Option<&'a String> {
+        match self.request_uri {
+            AbsolutePath(ref path) => Some(path),
+            _ => None
+        }
+    }
+}
+
