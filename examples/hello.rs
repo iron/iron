51486f047e5714ae88dec0ea2f2ea9070717dce4
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-20 12:01:05 -0700

    (doc) Refactored to use new fn middleware.

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,26 +3,18 @@ extern crate iron;
 
 use std::io::net::ip::Ipv4Addr;
 
-use iron::{Iron, Alloy, Middleware, Request, Response, ServerT};
+use iron::{Iron, Alloy, Request, Response, ServerT};
 use iron::mixin::Serve;
 use iron::middleware::{Status, Continue};
 
-#[deriving(Clone)]
-struct HelloWorld;
-
-impl Middleware for HelloWorld {
-    fn enter(&mut self,
-             _request: &mut Request,
-             response: &mut Response,
-             _alloy: &mut Alloy) -> Status {
-        let _ = response.serve(::http::status::Ok, "Hello, world!");
-        Continue
-    }
+fn hello_world(_req: &mut Request, res: &mut Response, _alloy: &mut Alloy) -> Status {
+    let _ = res.serve(::http::status::Ok, "Hello, world!");
+    Continue
 }
 
 fn main() {
     let mut server: ServerT = Iron::new();
-    server.smelt(HelloWorld);
+    server.smelt(hello_world);
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
 
