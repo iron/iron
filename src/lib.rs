3d7871083003f1de7e12479e58bf3d906b363308
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-19 17:16:52 -0700

    (feat) Added mount! macro.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //#![deny(missing_doc)]
 #![deny(unused_result, unused_result, unnecessary_qualification,
         non_camel_case_types, unused_variable, unnecessary_typecast)]
+#![feature(macro_rules)]
 
 extern crate http;
 extern crate iron;
