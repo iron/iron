2e8b10d74221b6b3491c33cf383c263408715119
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-22 13:16:11 -0700

    (feat) Implemented Middleware for Persistent.

--- src/persistent.rs
+++ src/persistent.rs
@@ -1,3 +1,5 @@
+use iron::{Request, Response, Middleware, Alloy};
+use iron::middleware::{Status, Continue};
 use std::sync::{Arc, Mutex};
 
 pub struct Persistent<Data, Phantom> {
@@ -9,6 +11,14 @@ impl<D: Send, P> Clone for Persistent<D, P> {
         Persistent {
             data: self.data.clone()
         }
+    }
+}
+
+impl<D: Send, P> Middleware for Persistent<D, P> {
+    fn enter(&mut self, _: &mut Request,
+             _: &mut Response, alloy: &mut Alloy) -> Status {
+        alloy.insert::<Persistent<D, P>>(self.clone());
+        Continue
     }
 }
 
