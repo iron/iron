50cf55c139dc0cef7ca7b17da646a7202cefa63e
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-22 13:07:57 -0700

    (feat) Implemented Clone for Sendable data.

--- src/persistent.rs
+++ src/persistent.rs
@@ -1,1 +1,14 @@
-pub struct Persistent;
+use std::sync::{Arc, Mutex};
+
+pub struct Persistent<Data, Phantom> {
+    data: Arc<Mutex<Data>>
+}
+
+impl<D: Send, P> Clone for Persistent<D, P> {
+    fn clone(&self) -> Persistent<D, P> {
+        Persistent {
+            data: self.data.clone()
+        }
+    }
+}
+
