adf3e54189bf89797ccc6388e0b26c789d725a6d
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-23 13:05:24 -0700

    (feat) Added public constructor.

--- src/shared.rs
+++ src/shared.rs
@@ -11,6 +11,14 @@ pub struct Shared {
     pub middleware: Arc<Box<ShareableMiddleware + Send + Share>>
 }
 
+impl Shared {
+    pub fn new<S: ShareableMiddleware + Send + Share>(s: S) -> Shared {
+        Shared {
+            middleware: Arc::new(box s as Box<ShareableMiddleware + Send + Share>)
+        }
+    }
+}
+
 // Needed to hack name resolution in our Clone impl
 fn clone<T: Clone>(t: &T) -> T { t.clone() }
 
