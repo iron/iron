4dba9ec4fcaf8870f9f5c3d85c7994e2db58920f
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-24 16:01:31 -0700

    (doc) Documented crate and modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
 #![crate_id = "persistent"]
 #![license = "MIT"]
-//#![deny(missing_doc)]
+#![deny(missing_doc)]
 #![deny(unused_result, unused_result, unnecessary_qualification,
         non_camel_case_types, unused_variable, unnecessary_typecast)]
+
+//! A set of middleware for sharing data between requests in the Iron
+//! framework.
 
 extern crate iron;
 
 pub use persistent::Persistent;
 pub use shared::Shared;
 
+/// Exposes the `Persistent` middleware, for sharing a single piece of data
+/// between requests.
 pub mod persistent;
+
+/// Exposes the `Shared` middleware, which wraps `ShareableMiddleware` so that
+/// they are not cloned for each request and improves performance by removing
+/// unnecessary cloning of immutable data.
 pub mod shared;
+
+/// Exposes a `link_shared` method on `Chain` objects, providing a convenient
+/// way to add shared middleware using `Shared`.
 pub mod mixin;
 
