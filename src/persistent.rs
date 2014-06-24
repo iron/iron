b5d3a946212c966b96ac8b31ae995cc56c647565
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-24 15:19:06 -0700

    (fix) Removed duplicate definition of new.

--- src/persistent.rs
+++ src/persistent.rs
@@ -28,9 +28,3 @@ impl<D: Send + Share, P> Persistent<D, P> {
     }
 }
 
-impl<D: Send + Share, P> Persistent<D, P> {
-    pub fn new(data: D) -> Persistent<D, P> {
-        return Persistent { data: Arc::new(RWLock::new(data)) };
-    }
-}
-
