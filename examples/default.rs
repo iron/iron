10bc88983e63cfe2e2a8db23d9514951d7fd6823
theptrk <patrick.tran06@gmail.com>
2014-06-25 15:15:03 -0700

    (refactor) Updated to chain-link architecture

--- examples/default.rs
+++ examples/default.rs
@@ -3,13 +3,13 @@ extern crate logger;
 
 use std::io::net::ip::Ipv4Addr;
 
-use iron::{Iron, ServerT};
+use iron::{Iron, ServerT, Chain};
 
 use logger::Logger;
 
 fn main() {
     let logger = Logger::new(None);
     let mut server: ServerT = Iron::new();
-    server.smelt(logger);
+    server.chain.link(logger);
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
