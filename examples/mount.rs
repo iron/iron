6f25332779e5c6aa591e2522fec1ac710eec7336
zzmp <zmp@umich.edu>
2014-06-30 13:26:00 -0700

    (fix) Updated for FromFn.

--- examples/mount.rs
+++ examples/mount.rs
@@ -5,7 +5,7 @@ extern crate mount;
 use std::io::net::ip::Ipv4Addr;
 
 use iron::{Iron, Request, Response, Alloy, ServerT, Chain};
-use iron::middleware::{Status, Continue, Unwind};
+use iron::middleware::{Status, Continue, Unwind, FromFn};
 use iron::mixin::Serve;
 use http::status;
 use mount::Mount;
@@ -23,8 +23,8 @@ fn send_hello(_req: &mut Request, res: &mut Response,
 
 fn main() {
     let mut server: ServerT = Iron::new();
-    server.chain.link(Mount::new("/blocked", intercept));
-    server.chain.link(send_hello);
+    server.chain.link(Mount::new("/blocked", FromFn::new(intercept)));
+    server.chain.link(FromFn::new(send_hello));
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
 
