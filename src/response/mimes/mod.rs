996334cd5c003f35a6ad5c27043e7a6479071b5b
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-07-03 11:29:46 +0200

    (refactor) simplified content_type retrieval
    This commit changes the content_type retrieval
    to use the built in method `extension_str()` instead
    of the hand crafted Regex to figure out the
    content_type.
    
    It also adds a test.

--- src/response/mimes/mod.rs
+++ src/response/mimes/mod.rs
@@ -1,5 +1,3 @@
-use std::path::BytesContainer;
-
 use http::headers::content_type::MediaType;
 
 use self::mimegen::get_generated_content_type;
@@ -7,15 +5,14 @@ use self::mimegen::get_generated_content_type;
 mod mimegen;
 
 pub fn get_content_type(path: &Path) -> Option<MediaType> {
-    let path_str = path.container_as_str().unwrap();
-    let ext_pos = regex!(".[a-z0-9]+$").find(path_str);
-    let mut ext;
-    match ext_pos {
-        Some((start, _)) => {
-            ext = path_str.as_slice().slice_from(start);
-        },
-        None => return None
-    }
+    path.extension_str().and_then(get_generated_content_type)
+}
 
-    get_generated_content_type(ext)
+#[test]
+fn matches_content_type () {
+    let path = &Path::new("test.txt");
+    let content_type = get_content_type(path).unwrap();
+
+    assert_eq!(content_type.type_.as_slice(), "text");
+    assert_eq!(content_type.subtype.as_slice(), "plain");
 }
