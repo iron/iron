95e9aeea6ff9fc2612791127039a023e7f0f8d0b
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-24 12:02:28 -0700

    (doc) Added README example and overview to crate-doc-comment.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,41 @@
 
 #![feature(macro_rules, phase, globs)]
 //! The main crate for the Iron library.
+//!
+//! Iron is a high level web framework built in and for Rust.
+//!
+//! Iron does not come bundled with any middleware - instead, Iron
+//! provides a robust and efficient framework for creating and
+//! plugging in middleware.
+//!
+//! Obligatory example:
+//!
+//! ```rust
+//! #[deriving(Clone)]
+//! pub struct ResponseTime { entry: u64 }
+//!
+//! impl ResponseTime { fn new() -> ResponseTime { ResponseTime { entry: 0u64 } } }
+//!
+//! // This Trait defines middleware.
+//! impl MiddleWare for ResponseTime {
+//!     fn enter(&mut self, _: &mut Request, _: &mut Response, _: &mut Alloy) -> Status {
+//!         self.entry = precise_time_ns();
+//!         Continue // Continue to other middleware in the stack
+//!     }
+//!
+//!     fn exit(&mut self, _: &mut Request, _: &mut Response, _: &mut Alloy) -> Status {
+//!         let delta = precise_time_ns() - self.entry;
+//!         println!("Request took {} ms.", (delta as f64) / 100000.0)
+//!         Continue
+//!     }
+//! }
+//!
+//! // ...
+//! server.chain.link(ResponseTime::new());
+//! // ...
+//! ```
+//!
+
 
 extern crate regex;
 #[phase(plugin)] extern crate regex_macros;
