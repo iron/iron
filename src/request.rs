143c41cbde92b31748712df46c57dffa38a3f65c
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-23 23:00:26 -0700

    (doc) Documented Request wrapper.

--- src/request.rs
+++ src/request.rs
@@ -6,21 +6,28 @@ use http::headers::request::HeaderCollection;
 use http::method::Method;
 pub use HttpRequest = http::server::request::Request;
 
+/// The `Request` given to all `Middleware`
 pub struct Request {
+    /// The request's requested url.
     pub url: String,
 
+    /// The originating address of the request.
     pub remote_addr: Option<SocketAddr>,
 
+    /// The request headers
     pub headers: Box<HeaderCollection>,
 
+    /// The request body
     pub body: String,
 
+    /// The request method
     pub method: Method,
 }
 
-    /// Get a mutable url from a Request
 impl Request {
+    /// Create a request from an HttpRequest.
     ///
+    /// This constructor consumes the HttpRequest.
     pub fn from_http(req: HttpRequest) -> Option<Request> {
         match req.request_uri {
             AbsolutePath(path) => {
