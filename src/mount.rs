63829c9ade310f116454d77810b71a0da960ce94
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-26 16:47:02 -0700

    (fix) Updated for new Request and Response.

--- src/mount.rs
+++ src/mount.rs
@@ -1,7 +1,6 @@
 use regex::Regex;
 
-use iron::{Middleware, Request, Response, Alloy, Chain, Status, Continue};
-use iron::mixin::GetUrl;
+use iron::{Middleware, Request, Response, Alloy, Status, Continue};
 
 /// Exposes the original, unmodified path to be stored in an Alloy.
 pub struct OriginalUrl(pub String);
@@ -12,29 +11,20 @@ pub struct OriginalUrl(pub String);
 ///
 /// Mounted middleware will receive a modified version of the request
 /// url with the mount pattern removed from the start of the url.
-pub struct Mount {
+#[deriving(Clone)]
+pub struct Mount<M> {
     route: String,
     matches: Regex,
-    middleware: Box<Middleware + Send>
+    middleware: M
 }
 
-impl Clone for Mount {
-    fn clone(&self) -> Mount {
-        Mount {
-            route: self.route.clone(),
-            matches: self.matches.clone(),
-            middleware: self.middleware.clone_box()
-        }
-    }
-}
-
-impl Mount {
+impl<M: Middleware + Send> Mount<M> {
     /// Creates a new instance of `Mount` mounting the given instance of Iron
     /// on the given path.
-    pub fn new<M: Middleware + Send>(route: &str, middleware: M) -> Mount {
+    pub fn new(route: &str, middleware: M) -> Mount<M> {
         Mount {
             route: route.to_string(),
-            middleware: box middleware,
+            middleware: middleware,
             matches: to_regex(route)
         }
     }
@@ -44,53 +34,29 @@ fn to_regex(route: &str) -> Regex {
     Regex::new("^".to_string().append(route).as_slice()).unwrap()
 }
 
-impl Middleware for Mount {
+impl<M: Middleware + Send> Middleware for Mount<M> {
     fn enter(&mut self,
              req: &mut Request,
              res: &mut Response,
              alloy: &mut Alloy) -> Status {
-        // This method is ugly, but it is hard to make it pretty
-        // because we can't both borrow path from inside of request
-        // while allowing chain.dispatch to borrow it as mutable.
-
-        match req.url() {
-           Some(path) => {
-               // Short circuit if we don't match.
-                if !self.matches.is_match(path.as_slice()) {
-                    return Continue;
-                }
-           },
-           // Short circuit if this is not an AbsolutePath.
-           None => { return Continue; }
+        if !self.matches.is_match(req.url.as_slice()) {
+            return Continue;
         }
 
         // We are a match, so fire off to our child instance.
-        match req.url_mut() {
-            Some(path) => {
-                // Insert the unmodified path into the alloy.
-                match alloy.find::<OriginalUrl>() {
-                    Some(_) => (),
-                    None => alloy.insert(OriginalUrl(path.clone()))
-                }
-                *path = path.as_slice().slice_from(self.route.len()).to_string();
-            },
-            // Absolutely cannot happen because of our previous check,
-            // but this is here just to be careful.
-            None => { return Continue; }
-        } // Previous borrow of req ends here.
+        // Insert the unmodified path into the alloy.
+        match alloy.find::<OriginalUrl>() {
+            Some(_) => (),
+            None => alloy.insert(OriginalUrl(req.url.clone()))
+        }
+        req.url = req.url.as_slice().slice_from(self.route.len()).to_string();
 
-        // So we can borrow it again here.
         let terminator = self.middleware.enter(req, res, alloy);
         let _ = self.middleware.exit(req, res, alloy);
 
         // And repair the damage here, for future middleware
-        match req.url_mut() {
-            Some(path) => {
-                *path = self.route.clone().append(path.as_slice());
-            },
-            // This really, really should never happen.
-            None => { fail!("The impossible happened."); }
-        }
+        let &OriginalUrl(ref original) = alloy.find::<OriginalUrl>().unwrap();
+        req.url = original.clone();
 
         // We dispatched the request, so return the terminator.
         terminator
