28f949c70086882ef5efaa588928e02098175448
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-08 12:16:41 -0700

    (fix) Updated examples.

--- examples/mount.rs
+++ examples/mount.rs
@@ -5,23 +5,22 @@ extern crate mount;
 use std::io::net::ip::Ipv4Addr;
 
 use http::status;
-use iron::{Iron, Request, Response, Alloy, Server, Chain, Status, Continue, Unwind, FromFn};
+use iron::{Iron, Request, Response, Server, Chain, Status, Continue, Unwind, FromFn};
 use mount::Mount;
 
-fn intercept(_req: &mut Request, _res: &mut Response,
-             _alloy: &mut Alloy) -> Status {
+fn intercept(_: &mut Request, res: &mut Response) -> Status {
+    let _ = res.serve(status::Ok, "Blocked!");
     Unwind
 }
 
-fn send_hello(_req: &mut Request, res: &mut Response,
-              _alloy: &mut Alloy) -> Status {
+fn send_hello(_: &mut Request, res: &mut Response) -> Status {
     let _ = res.serve(status::Ok, "Hello!");
     Continue
 }
 
 fn main() {
     let mut server: Server = Iron::new();
-    server.chain.link(Mount::new("/blocked", FromFn::new(intercept)));
+    server.chain.link(Mount::new(&["blocked"], FromFn::new(intercept)));
     server.chain.link(FromFn::new(send_hello));
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
