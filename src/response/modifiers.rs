df6d4567ae4095e10cb75b5ecb3014a3c0f9cb53
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-01 18:07:13 -0700

    (feat) Added a Body modifier, for efficiently setting a response body.

---
+++ src/response/modifiers.rs
@@ -0,0 +1,87 @@
+//! Defines a series of convenience modifiers for editing Responses
+
+use std::str::StrAllocating;
+use std::io::{File, MemReader};
+use std::path::Path;
+
+use modifier::Modifier;
+use http::headers::content_type::MediaType;
+use content_type::get_content_type;
+use {Response};
+
+/// A response modifier for seeting the body of a response.
+pub struct Body<B: Bodyable>(B);
+
+impl<B: Bodyable> Modifier<Response> for Body<B> {
+    #[inline]
+    fn modify(self, mut res: Response) -> Response {
+        let Body(b) = self;
+        b.set_body(&mut res);
+        res
+    }
+}
+
+/// A modifier that can be used to set the body of a response.
+pub trait Bodyable {
+    /// Set the body of this response, possibly also setting headers.
+    fn set_body(self, res: &mut Response);
+}
+
+impl Bodyable for Box<Reader + Send> {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        res.body = Some(self);
+    }
+}
+
+impl Bodyable for String {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        self.into_bytes().set_body(res);
+    }
+}
+
+impl Bodyable for Vec<u8> {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        res.headers.content_length = Some(self.len());
+        res.body = Some(box MemReader::new(self) as Box<Reader + Send>);
+    }
+}
+
+impl<'a> Bodyable for &'a str {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        self.into_string().set_body(res);
+    }
+}
+
+impl<'a> Bodyable for &'a [u8] {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        self.to_vec().set_body(res);
+    }
+}
+
+impl Bodyable for File {
+    #[inline]
+    fn set_body(self, res: &mut Response) {
+        // Also set the content type.
+        res.headers.content_type = self.path().extension_str().and_then(get_content_type);
+        res.body = Some(box self as Box<Reader + Send>);
+    }
+}
+
+impl Bodyable for Path {
+    /// Set the body to the contents of the File at this path.
+    ///
+    /// ## Panics
+    ///
+    /// Panics if there is no file at the passed-in Path.
+    fn set_body(self, res: &mut Response) {
+        File::open(&self)
+            .ok().expect(format!("No such file: {}", self.display()).as_slice())
+            .set_body(res);
+    }
+}
+
