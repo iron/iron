a3a890f784bd707ebdded8d8947ce2a7a448970c
Kai Lieth <dubokk15@gmail.com>
2015-07-20 07:02:39 -0400

    Implement std::error::Error for PersistentError

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern crate plugin;
 use iron::{Request, Response, BeforeMiddleware, AfterMiddleware, IronResult};
 use iron::typemap::Key;
 use std::sync::{Arc, RwLock, Mutex};
+use std::{error,fmt};
 use plugin::Plugin;
 
 /// The type that can be returned by `eval` to indicate error.
@@ -17,6 +18,20 @@ use plugin::Plugin;
 pub enum PersistentError {
     /// The value was not found.
     NotFound
+}
+
+impl fmt::Display for PersistentError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt::Display::fmt(self, f)
+    }
+}
+
+impl error::Error for PersistentError {
+    fn description(&self) -> &str {
+        match *self {
+            PersistentError::NotFound => "Value not found in extensions."
+        }
+    }
 }
 
 /// Middleware for data that persists between requests with read and write capabilities.
