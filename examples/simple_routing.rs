943bc4c85276955e2bbc9ca83840772eefd98c31
Paul Dib <paul.dib@orange.fr>
2015-10-06 14:01:12 +0200

    (doc) Added a simple routing example

---
+++ examples/simple_routing.rs
@@ -0,0 +1,48 @@
+extern crate iron;
+
+use std::collections::HashMap;
+
+use iron::prelude::*;
+use iron::{Handler};
+use iron::status;
+
+struct Router {
+    // Routes here are simply matched with the url path.
+    routes: HashMap<String, Box<Handler>>
+}
+
+impl Router {
+    fn new() -> Self {
+        Router { routes: HashMap::new() }
+    }
+
+    fn add_route<H>(&mut self, path: String, handler: H) where H: Handler {
+        self.routes.insert(path, Box::new(handler));
+    }
+}
+
+impl Handler for Router {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        match self.routes.get(&req.url.path.join("/")) {
+            Some(handler) => handler.handle(req),
+            None => Ok(Response::with(status::NotFound))
+        }
+    }
+}
+
+fn main() {
+    let mut router = Router::new();
+    router.add_route("hello".to_string(), |_: &mut Request| {
+        Ok(Response::with("Hello world !"))
+    });
+
+    router.add_route("hello/again".to_string(), |_: &mut Request| {
+       Ok(Response::with("Hello again !")) 
+    });
+
+    router.add_route("error".to_string(), |_: &mut Request| {
+       Ok(Response::with(status::BadRequest))
+    });
+
+    Iron::new(router).http("localhost:3000").unwrap();
+}
