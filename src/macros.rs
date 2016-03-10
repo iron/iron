f1cc306336fcfed3fbece372858ad7bcfa1f56c2
Danilo Bargen <mail@dbrgn.ch>
2016-03-10 11:50:30 +0100

    Expand docs on itry!

--- src/macros.rs
+++ src/macros.rs
@@ -2,13 +2,19 @@
 //!
 //! The idea is to use `itry` for internal server operations which can't be recovered from, and
 //! `iexpect` for validating user input. Note that this kind of usage is completely non-normative.
-//! Feedback about actual usability and usage is apprechiated.
+//! Feedback about actual usability and usage is appreciated.
 
-/// Like try!(), but wrapping the error value in `IronError`. To be used in request handlers.
+/// Like `try!()`, but wraps the error value in `IronError`. To be used in
+/// request handlers.
+///
+/// The second (optional) parameter is any [modifier](modifiers/index.html).
+/// The default modifier is `status::InternalServerError`.
+///
 ///
 /// ```ignore
 /// let f = itry!(fs::File::create("foo.txt"), status::BadRequest);
-/// let f = itry!(fs::File::create("foo.txt"));  // Default modifier is status::InternalServerError
+/// let f = itry!(fs::File::create("foo.txt"), (status::NotFound, "Not Found"));
+/// let f = itry!(fs::File::create("foo.txt"));  // HTTP 500
 /// ```
 ///
 #[macro_export]
@@ -22,8 +28,8 @@ macro_rules! itry {
     })
 }
 
-/// Unwrap the given Option or return a Ok(Response::new()) with the given modifier. The default
-/// modifier is status::BadRequest.
+/// Unwrap the given `Option` or return a `Ok(Response::new())` with the given
+/// modifier. The default modifier is `status::BadRequest.
 #[macro_export]
 macro_rules! iexpect {
     ($option:expr) => (iexpect!($option, $crate::status::BadRequest));
