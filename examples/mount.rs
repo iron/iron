d1a9b3f9bb033ccde3bf4a59a49fdb0b1e15a735
Hike Li <hikelee@gmail.com>
2016-07-12 18:47:07 +0800

    use iron 0.4 apis in examples

--- examples/mount.rs
+++ examples/mount.rs
@@ -7,12 +7,12 @@ use iron::status;
 use mount::Mount;
 
 fn send_hello(req: &mut Request) -> IronResult<Response> {
-    println!("Running send_hello handler, URL path: {:?}", req.url.path);
+    println!("Running send_hello handler, URL path: {:?}", req.url.path());
     Ok(Response::with((status::Ok, "Hello!")))
 }
 
 fn intercept(req: &mut Request) -> IronResult<Response> {
-    println!("Running intercept handler, URL path: {:?}", req.url.path);
+    println!("Running intercept handler, URL path: {:?}", req.url.path());
     Ok(Response::with((status::Ok, "Blocked!")))
 }
 
@@ -22,4 +22,3 @@ fn main() {
 
     Iron::new(mount).http("localhost:3000").unwrap();
 }
-
