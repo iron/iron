a97f6b1e79e9eb1fe271f90d6d762eb9191ef8d2
INORIA <inoria@goldfish-bowl.jp>
2017-10-04 03:54:27 +0900

    (doc) fix SSL cert generating command error (#561)

--- examples/https.rs
+++ examples/https.rs
@@ -8,7 +8,7 @@
 //
 // ```bash
 // openssl req -x509 -newkey rsa:4096 -nodes -keyout localhost.key -out localhost.crt -days 3650
-// openssl pkcs12 -export -out identity.p12 -inkey localhost.key -in localhost.crt -password mypass
+// openssl pkcs12 -export -out identity.p12 -inkey localhost.key -in localhost.crt -password pass:mypass
 //
 // ```
 
